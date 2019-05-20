e11853235071d1dec59fed382f8c23a8f5368353
AugustineOchieng <gusochieng@gmail.com>
2019-05-20 11:25:40 +0300

    Register user apps in installed apps

--- instagramme/settings.py
+++ instagramme/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4'
+    'users.apps.UsersConfig',
+    'crispy_forms',
+    'bootstrap4',
     'myinsta',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -77,8 +79,10 @@ WSGI_APPLICATION = 'instagramme.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagus',
+        'USER': 'moringa',
+        'PASSWORD':'gus9999#'
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -120,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
