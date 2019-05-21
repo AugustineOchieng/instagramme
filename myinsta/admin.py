1b7a340e6ba949935b3cfa29044fffe57365d974
AugustineOchieng <gusochieng@gmail.com>
2019-05-21 17:17:35 +0300

    Register Comments class

--- myinsta/admin.py
+++ myinsta/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image, Profile, Comments
 
 # Register your models here.
 admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comments)
 
