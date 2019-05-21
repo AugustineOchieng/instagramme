d2c97df581bd3945a4a055a0c2014717b7be41a5
AugustineOchieng <gusochieng@gmail.com>
2019-05-21 06:45:59 +0300

    Configure routes to the home page, login and registration

---
+++ myinsta/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns=[
+    url(r'^$', views.home, name='instahomepage'),
+   
+    
+    
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
