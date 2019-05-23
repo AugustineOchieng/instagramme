dadccdaf8ebd0587cf5e29ebbb0e440a6baf5978
AugustineOchieng <gusochieng@gmail.com>
2019-05-23 10:07:30 +0300

    Add class tests for mos=dels

--- myinsta/models.py
+++ myinsta/models.py
@@ -33,3 +33,67 @@ class Profile(models.Model):
         return self.bio
 
 
+class Image(models.Model):
+    image_name = models.CharField(max_length=30)
+    image_caption = models.CharField(max_length=100)
+    image = models.ImageField(upload_to = 'studio/', default="")
+    date_posted = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(User,on_delete = models.CASCADE,null = True)
+    poster_id=models.IntegerField(default=0)
+    
+    @classmethod
+    def get_images(cls):
+        image = cls.objects.all()
+        return image
+
+    @classmethod
+    def get_image(cls, image_id):
+        single_image = cls.objects.get(id=image_id)
+        return single_image
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        Image.objects.filter().delete()
+
+    def update_image_caption(self, update):
+        self.image_caption = update
+        self.save()
+
+    class Meta:
+        ordering = ['-id']
+
+    def __str__(self):
+      return self.image_name
+
+
+class Comments(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    commented = models.CharField(max_length = 200)
+    
+    @classmethod
+    def get_comments(cls):
+        comment = cls.objects.all()
+        return comment
+
+    def save_comment(self):
+        self.save()     
+
+    def delete_comment(self):
+        Image.objects.filter().delete()
+
+    def update_comment(self, update):
+        self.comment_message = update
+        self.save()
+
+    def get_comments_by_images(cls, id):
+        comments = Comments.objects.filter(image_pk = id)
+        return comments
+    
+    def __str__(self):
+      comments=Comments.objects.filter(image_pk = id)
+      return self.commented
+
+
