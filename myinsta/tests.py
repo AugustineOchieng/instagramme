f85a1ba6f2cac98106a6379e12fac933b47875a2
AugustineOchieng <gusochieng@gmail.com>
2019-05-23 10:06:54 +0300

    Add unittest for Image class

--- myinsta/tests.py
+++ myinsta/tests.py
@@ -29,3 +29,43 @@ class ProfileTestClass(TestCase):
         self.assertTrue(len(profiles) == 0)
 
 
+class ImageTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        # Profile class test
+        self.kevin = Profile( bio = "I love traveling", userId = 1)
+
+        # Image class Test
+        self.image = Image(image = " ",image_name="nature goodness",image_caption = "", publish_date = "10/2/2019", poster_id = 2)
+        self.image.save_image()
+
+
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+    # Testing Save method
+    def test_save_method(self):
+        self.image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)>0)
+
+    # Testing Delete method
+    def test_delete_method(self):
+        self.image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) == 0)
+
+
+    # Testing getting image by id 
+    def test_get_image(self):
+        images = Image.get_image(self.image.id)
+        self.assertTrue(images == self.image)
+
+    # Testing Update method
+    def test_update_method(self):
+        image = Image.get_image(self.image.id)
+        image.update_image_caption("new_image")
+        image = Image.get_image(self.image.id)
+        self.assertTrue(image.image == "new_image")
